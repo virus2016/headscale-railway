7ad443efd51ccd2e84ef361ed9984951d9255c3e
Calum MacRae <calum.macrae@medayo.com>
2023-12-30 23:16:38 +0000

    added entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,14 @@ done
 echo "HeadScale is up and running."
 
 # Check for existing API keys
-if ! headscale apikeys list | grep -q "No API keys found"; then
-    echo "API keys already exist. No new key will be created."
-else
+# if ! headscale apikeys list | grep -q "No API keys found"; then
+    # echo "API keys already exist. No new key will be created."
+# else
     # Create a new API key with 90 days expiration
-    headscale apikeys create
-    echo "New API key created."
-fi
+headscale apikeys list
+headscale apikeys create
+echo "New API key created."
+# fi
 
 # Keep the container running
 tail -f /dev/null
