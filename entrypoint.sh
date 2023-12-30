1e77ae432ec15b7e09ddb83cb09303f75aa00175
Calum MacRae <calum.macrae@medayo.com>
2023-12-30 23:22:22 +0000

    keep headscale at the front

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-# Start HeadScale service in the background
+# Function to check and create API keys
+function manage_api_keys {
+    # Wait for HeadScale to be operational
+    while ! curl -s http://localhost:8080 > /dev/null; do
+        sleep 1 # wait for 1 second before checking again
+    done
+
+    # Check for existing API keys
+    if headscale apikeys list | grep -q "ID \| Prefix \| Expiration \| Created"; then
+        echo "API keys already exist. No new key will be created."
+    else
+        # Create a new API key with 90 days expiration
+        new_key=$(headscale apikeys create)
+        echo "New API key created: $new_key"
+        # Optionally, save the new key to a file
+        # echo "$new_key" > /path/to/apikey.txt
+        # echo "New API key saved to /path/to/apikey.txt."
+    fi
+}
+
+# Start the HeadScale service in the foreground
+echo "Starting HeadScale service..."
 headscale serve &
 
-# Wait for HeadScale to be operational
-while ! nc -z localhost 8080; do   
-  sleep 1 # wait for 1 second before checking again
-done
+# Manage API keys in the background
+manage_api_keys &
 
-echo "HeadScale is up and running."
-
-# Check for existing API keys
-# if ! headscale apikeys list | grep -q "No API keys found"; then
-    # echo "API keys already exist. No new key will be created."
-# else
-    # Create a new API key with 90 days expiration
-headscale apikeys list
-headscale apikeys create
-echo "New API key created."
-# fi
-
-# Keep the container running
-tail -f /dev/null
+# Bring HeadScale serve back into the foreground
+wait %1
