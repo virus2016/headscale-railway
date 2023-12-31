a37cc234c98c28593a0b1c476bf10648948f552f
Calum MacRae <calum.macrae@medayo.com>
2023-12-31 00:18:14 +0000

    oppps

--- caddy/entrypoint.sh
+++ caddy/entrypoint.sh
@@ -5,7 +5,7 @@ cat <<EOF > /etc/caddy/Caddyfile
 :80 {
     reverse_proxy /web* http://headscale-ui.railway.internal:8080
 
-    reverse_proxy * http://headscale-production.up.railway.app:8080
+    reverse_proxy * http://headscale-railway.railway.internal:8080
 }
 EOF
 
